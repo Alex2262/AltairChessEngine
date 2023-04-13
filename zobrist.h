236a0d57d21c294bca5657231e05bd14ece7a449
Alexander Tian <alex2262alexander@gmail.com>
2023-04-12 20:36:57 -0400

    SF PRAND

--- zobrist.h
+++ zobrist.h
@@ -9,17 +9,25 @@
 #include "constants.h"
 
 
-inline uint64_t rand_uint64() {
-    uint64_t r = 0;
-    for (int i=0; i<64; i++) {
-        r = r*2 + rand()%2;
+//A psuedorandom number generator
+//Source: Stockfish
+class PRNG {
+
+public:
+    uint64_t s;
+    uint64_t rand64() {
+        s ^= s >> 12, s ^= s << 25, s ^= s >> 27;
+        return s * 2685821657736338717LL;
     }
-    return r;
-}
+
+    PRNG(uint64_t seed) : s(seed) {}
+};
 
 class ZobristKeysClass {
 
 public:
+
+    PRNG prng = PRNG(31415926);
 
     HASH_TYPE piece_hash_keys[12][64] {};
     HASH_TYPE ep_hash_keys[64] {};
@@ -29,19 +37,19 @@ public:
     ZobristKeysClass() {
         for (auto & piece_hash_key : piece_hash_keys) {
             for (uint64_t& j : piece_hash_key) {
-                j = rand_uint64();
+                j = prng.rand64();
             }
         }
 
         for (uint64_t& ep_hash_key : ep_hash_keys) {
-            ep_hash_key = rand_uint64();
+            ep_hash_key = prng.rand64();
         }
 
         for (uint64_t& castle_hash_key : castle_hash_keys) {
-            castle_hash_key = rand_uint64();
+            castle_hash_key = prng.rand64();
         }
 
-        side_hash_key = rand_uint64();
+        side_hash_key = prng.rand64();
     }
 };
 
