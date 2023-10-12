1af7161e153faf2e25af4722995890c134f848fa
iudwgerte <a09701070@gmail.com>
2023-10-13 01:03:47 +0800

    Bench: 6345596

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -11,7 +11,7 @@
 
 #include "incbin.h"
 
-INCBIN(nnue, "src/nnue-1024-epoch20.bin");
+INCBIN(nnue, "src/net-epoch30.bin");
 
 const NNUE_Params &nnue_parameters = *reinterpret_cast<const NNUE_Params *>(gnnueData);
 
