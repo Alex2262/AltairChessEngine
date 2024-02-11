7f522e9149660295e843da3b61e92eb7c8e61298
alexandertian <alex2262alexander@gmail.com>
2024-02-11 14:52:34 -0500

    pawn correction history Bench: 9178117

--- src/search.cpp
+++ src/search.cpp
@@ -414,7 +414,7 @@ SCORE_TYPE qsearch(Engine& engine, SCORE_TYPE alpha, SCORE_TYPE beta, PLY_TYPE d
     SCORE_TYPE static_eval = engine.probe_tt_evaluation(position.hash_key);
     if (static_eval == NO_EVALUATION) static_eval = engine.evaluate<NNUE>(thread_id);
 
-    SCORE_TYPE adjusted_eval = static_eval + thread_state.get_eval_correction() * psc_adjustment_scale;
+    SCORE_TYPE adjusted_eval = static_eval + static_cast<SCORE_TYPE>(thread_state.get_eval_correction() * psc_adjustment_scale);
     SCORE_TYPE best_score = adjusted_eval;
 
     // Return the best score if we have reached a stand-pat, or we have reached the maximum depth
