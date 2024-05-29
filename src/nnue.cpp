c8745f02ec0034d2312cb2e68d1fb98d0c714e9c
alexandertian <alex2262alexander@gmail.com>
2024-05-29 10:17:40 -0400

    New 1024 HL Net at 987 epochs Bench: 8865760

--- src/nnue.cpp
+++ src/nnue.cpp
@@ -12,8 +12,8 @@
 
 #include "incbin.h"
 
-INCBIN(nnue, "src/altair-20240524-800.bin");
-// INCBIN(nnue, "/Users/alexandertian/CLionProjects/Altair/src/altair-20240524-800.bin");
+INCBIN(nnue, "src/altair-20240524-987.bin");
+// INCBIN(nnue, "/Users/alexandertian/CLionProjects/Altair/src/altair-20240524-987.bin");
 
 const NNUE_Params &original_nnue_parameters = *reinterpret_cast<const NNUE_Params *>(gnnueData);
 const NNUE_Params nnue_parameters = get_nnue_parameters();
