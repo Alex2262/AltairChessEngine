76499c5a7ed2c8249ae9d3bb72504896f791bb13
Alex2262 <alex2262alexander@gmail.com>
2026-01-02 11:21:48 -0500

    Formatting fix Bench: 7317545

--- src/search.cpp
+++ src/search.cpp
@@ -425,11 +425,11 @@ Score qsearch(Engine& engine, Score alpha, Score beta, Ply depth, int thread_id)
     }
 
     // Probe the transposition table
-    TT_Entry   tt_entry{};
-    short      tt_return_type = engine.probe_tt_entry_q(thread_id, position.hash_key, alpha, beta, tt_entry);
-    Move       tt_move        = tt_entry.move;
-    bool       tt_pv          = tt_entry.pv_node || pv_node;
-    Score tt_value       = tt_entry.score;
+    TT_Entry tt_entry{};
+    short    tt_return_type = engine.probe_tt_entry_q(thread_id, position.hash_key, alpha, beta, tt_entry);
+    Move     tt_move        = tt_entry.move;
+    bool     tt_pv          = tt_entry.pv_node || pv_node;
+    Score    tt_value       = tt_entry.score;
 
     if (tt_return_type == RETURN_HASH_SCORE) {
         return tt_value;
@@ -473,7 +473,7 @@ Score qsearch(Engine& engine, Score alpha, Score beta, Ply depth, int thread_id)
 
     // Variables for getting information about the best score / best move
     Score best_score = eval;
-    Move       best_move  = NO_MOVE;
+    Move  best_move  = NO_MOVE;
 
     // Search loop
     for (generator.reset_qsearch(tt_move); generator.stage != Stage::Terminated;) {
@@ -620,12 +620,12 @@ Score negamax(Engine& engine, Score alpha, Score beta, Ply depth, bool do_null, 
     position.set_state(position.state_stack[thread_state.search_ply], thread_state.fifty_move);
 
     // TT probing
-    TT_Entry   tt_entry{};
-    short      tt_hash_flag   = HASH_FLAG_UPPER;
-    short      tt_return_type = engine.probe_tt_entry(thread_id, position.hash_key, alpha, beta, depth, tt_entry);
-    Move       tt_move        = tt_entry.move;
-    bool       tt_pv          = tt_entry.pv_node || pv_node;
-    Score tt_value       = tt_entry.score;
+    TT_Entry tt_entry{};
+    short    tt_hash_flag   = HASH_FLAG_UPPER;
+    short    tt_return_type = engine.probe_tt_entry(thread_id, position.hash_key, alpha, beta, depth, tt_entry);
+    Move     tt_move        = tt_entry.move;
+    bool     tt_pv          = tt_entry.pv_node || pv_node;
+    Score    tt_value       = tt_entry.score;
 
     /*
      * TT cutoffs
@@ -766,7 +766,7 @@ Score negamax(Engine& engine, Score alpha, Score beta, Ply depth, bool do_null, 
 
     // Best score for fail soft, and best move for tt
     Score best_score = -SCORE_INF;
-    Move       best_move  = NO_MOVE;
+    Move  best_move  = NO_MOVE;
 
     // Other information for pruning / reductions
     int alpha_raised_count = 0;
